093c06285e2847a8ef580058785fcb671573ecbe
Andrey Tsatsyn <tsatsynav@yandex.ru>
2024-03-27 00:31:44 +0300

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,11 @@ const concat = require("gulp-concat-css");
 const plumber = require("gulp-plumber");
 const del = require("del");
 const browserSync = require("browser-sync").create();
+const postcss = require("gulp-postcss");
+const autoprefixer = require("autoprefixer");
+const mediaquery = require("postcss-combine-media-query");
+const cssnano = require("cssnano");
+const htmlMinify = require("html-minifier");
 
 function serve() {
   browserSync.init({
@@ -13,18 +18,37 @@ function serve() {
 }
 
 function html() {
+  const options = {
+    removeComments: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortClassName: true,
+    useShortDoctype: true,
+    collapseWhitespace: true,
+    minifyCSS: true,
+    keepClosingSlash: true,
+  };
   return gulp
     .src("./src/**/*.html")
     .pipe(plumber())
+    .on("data", function (file) {
+      const buferFile = Buffer.from(
+        htmlMinify.minify(file.contents.toString(), options)
+      );
+      return (file.contents = buferFile);
+    })
     .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }));
 }
 
 function css() {
+  const plugins = [autoprefixer(), mediaquery(), cssnano()];
   return gulp
     .src("./src/**/*.css")
     .pipe(plumber())
     .pipe(concat("bundle.css"))
+    .pipe(postcss(plugins))
     .pipe(gulp.dest("dist/"))
     .pipe(browserSync.reload({ stream: true }));
 }
