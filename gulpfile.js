bc5c6e4a1f5ac6a2c96c7fa1f2ec590ade1d4724
Andrey Tsatsyn <tsatsynav@yandex.ru>
2024-03-27 00:08:08 +0300

    added all functions in gulpfile, added exports, added new commands to package.js

--- gulpfile.js
+++ gulpfile.js
@@ -3,3 +3,86 @@ const concat = require("gulp-concat-css");
 const plumber = require("gulp-plumber");
 const del = require("del");
 const browserSync = require("browser-sync").create();
+
+function serve() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist",
+    },
+  });
+}
+
+function html() {
+  return gulp
+    .src("./src/**/*.html")
+    .pipe(plumber())
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function css() {
+  return gulp
+    .src("./src/**/*.css")
+    .pipe(plumber())
+    .pipe(concat("bundle.css"))
+    .pipe(gulp.dest("dist/"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function fonts() {
+  return gulp
+    .src("./src/fonts/**/*.{woff2,woff,ttf}")
+    .pipe(gulp.dest("dist/fonts"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function images() {
+  return gulp
+    .src("./src/images/**/*.{jpg,png,gif,ico,webp,avif}")
+    .pipe(gulp.dest("dist/images"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function svg() {
+  return gulp
+    .src("./src/svg/**/*.svg")
+    .pipe(gulp.dest("dist/svg"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function scripts() {
+  return gulp
+    .src("./src/scripts/**/*.js")
+    .pipe(gulp.dest("dist/scripts"))
+    .pipe(browserSync.reload({ stream: true }));
+}
+
+function clean() {
+  return del("dist");
+}
+
+const build = gulp.series(
+  clean,
+  gulp.parallel(html, css, fonts, images, svg, scripts)
+);
+
+function watchFiles() {
+  gulp.watch(["./src/**/*.html"], html);
+  gulp.watch(["./src/**/*.css"], css);
+  gulp.watch(["./src/fonts/**/*.{woff2,woff,ttf}"], fonts);
+  gulp.watch(["./src/images/**/*.{jpg,png,gif,ico,webp,avif}"], images);
+  gulp.watch(["./src/svg/**/*.svg"], svg);
+  gulp.watch(["./src/scripts/**/*.js"], scripts);
+}
+const watchapp = gulp.parallel(build, watchFiles, serve);
+
+exports.html = html;
+exports.css = css;
+exports.fonts = fonts;
+exports.images = images;
+exports.svg = svg;
+exports.scripts = scripts;
+exports.clean = clean;
+exports.build = build;
+exports.watchapp = watchapp;
+exports.default = watchapp;
